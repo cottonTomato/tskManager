b0679efcf6a00ea16055c7c889bd6c0c1c1f14cd
LanceHoenhymm <abhianv.pandey.1512@gmail.com>
2023-09-03 22:22:25 +0530

    add tasks router

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,9 @@
-const { express } = require('express');
-
+import express from "express";
 require('dotenv').config();
+
+const app = express();
+
+
+app.listen(process.env.PORT, function () {
+    console.log(`Server Listininga at ${process.env.PORT}`);
+});
