ac5ce9cea5d8ff98e606f919d479d37f68b13415
LanceHoenhymm <abhianv.pandey.1512@gmail.com>
2023-09-05 21:10:57 +0530

    added functionality for getAllTasks

--- src/controllers/tasks.ts
+++ src/controllers/tasks.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from 'express';
+import Task from '../models/tasks'
 
 function getAllTasks(req: Request, res: Response) {
     res.send('Job Sucess..');
@@ -16,8 +17,13 @@ function deleteTask(req: Request, res: Response) {
     res.send('Job Sucess..');
 }
 
-function createTask(req: Request, res: Response) {
-    res.send('Job Sucess..');
+async function createTask(req: Request, res: Response) {
+    try {
+        await Task.create(req.body);
+        res.status(200).json({ sucess: true, data: req.body});
+    } catch (err) {
+        res.status(500).json({ sucess: false, error: err });
+    }
 }
 
 
